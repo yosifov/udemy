e6dee472644588e7ec4bfaf5201588407d6edecf
Yosifov <k.yosifov@gmail.com>
2019-12-06 20:16:37 +0200

    Add Section 8 Coding Quiz

---
+++ CodingQuiz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CodingQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int left = 0;
+
+            while (left <= 2)
+            {
+                int right = 10;
+
+                while (right >= 6)
+                {
+                    Console.Write($"Outer: {left}");
+                    Console.Write("\t");
+                    Console.WriteLine($"Inner: {right}");
+
+                    right--;
+                }
+
+                left++;
+            }
+        }
+    }
+}
