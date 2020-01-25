8b37f8acfc11926a019ec98a34a32f0b6bee0c8f
Kamen Yosifov <k.yosifov@gmail.com>
2020-01-26 00:16:45 +0200

    Add Section Nineteen Navigation Quiz

---
+++ LocatingWebElements/NavigationQuiz.cs
@@ -0,0 +1,67 @@
+﻿namespace LocatingWebElements
+{
+    using System.IO;
+    using System.Reflection;
+
+    using NUnit.Framework;
+
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+
+    [TestFixture]
+    [Category("Navigation Quiz")]
+    public class NavigationQuiz
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.driver = this.GetWebDriver();
+        }
+
+        [Test]
+        public void GoToHomePage()
+        {
+            this.driver.Navigate().GoToUrl("https://ultimateqa.com/");
+
+            Assert.AreEqual("Home - Ultimate QA", this.driver.Title);
+        }
+
+        [Test]
+        public void GoToAutomationPage()
+        {
+            this.driver.Navigate().GoToUrl("https://ultimateqa.com/automation");
+
+            Assert.AreEqual("Automation Practice - Ultimate QA", this.driver.Title);
+        }
+
+        [Test]
+        public void GoToComplicatedPage()
+        {
+            this.driver.Navigate().GoToUrl("https://ultimateqa.com/automation");
+
+            var element = this.driver.FindElement(By.XPath("//a[contains(@href, '/complicated-page')]"));
+            element.Click();
+
+            Assert.AreEqual("Complicated Page - Ultimate QA", this.driver.Title);
+
+            this.driver.Navigate().Back();
+
+            Assert.AreEqual("Automation Practice - Ultimate QA", this.driver.Title);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.driver.Quit();
+        }
+
+        private IWebDriver GetWebDriver()
+        {
+            var pathToDriver = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            return new ChromeDriver(pathToDriver);
+        }
+    }
+}
